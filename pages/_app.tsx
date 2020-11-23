ffaab8cc9487e3c055d2ccd1cd7d611e9fd8db9f
coderinblack <kevinlu.email@gmail.com>
2020-11-22 21:01:17 -0800

    Responsive typography and navbar icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,16 @@ function MyApp({ Component, pageProps }: any) {
           name="description"
           content="news, opinion, articles, information"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <Component {...pageProps} />
+      <div className="antialiased box-content">
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,67 +2,109 @@ import React from 'react';
 import useSound from 'use-sound';
 import { Button } from '../components/button';
 
+const Navbar: React.FC = () => (
+  <nav className="absolute top-0 py-8 lg:py-10 flex items-center justify-between w-full px-5 sm:px-6 md:px-10 lg:pl-0 lg:pr-20 text-gray-700 dark:text-gray-100">
+    <ul className="flex items-center space-x-8">
+      <li className="inline-flex items-center font-sans font-medium">
+        <svg
+          className="w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+            clipRule="evenodd"
+          />
+          <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+        </svg>
+        Content
+      </li>
+      <li className="inline-flex items-center font-sans font-medium">
+        <svg
+          className="w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+        About
+      </li>
+      <li className="inline-flex items-center font-sans font-medium">
+        <svg
+          className="w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            clipRule="evenodd"
+          />
+        </svg>
+        Search
+      </li>
+    </ul>
+    <ul className="hidden sm:flex items-center space-x-6 ml-auto">
+      <li>
+        <button>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </li>
+      <li>
+        <button>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M5 3a1 1 0 000 2c5.523 0 10 4.477 10 10a1 1 0 102 0C17 8.373 11.627 3 5 3z" />
+            <path d="M4 9a1 1 0 011-1 7 7 0 017 7 1 1 0 11-2 0 5 5 0 00-5-5 1 1 0 01-1-1zM3 15a2 2 0 114 0 2 2 0 01-4 0z" />
+          </svg>
+        </button>
+      </li>
+    </ul>
+  </nav>
+);
+
 const Index: React.FC = () => {
   const [play, { stop, isPlaying }] = useSound('/assets/pronounce_pish.mp3');
 
   return (
     <main className="dark">
-      <header className="grid grid-cols-12 h-screen bg-gray-50 dark:bg-gray-900">
-        <div className="pt-32 pb-20 sm:py-36 lg:p-10 flex flex-col justify-center relative h-full pl-16 col-span-12 md:col-span-7">
-          <nav className="absolute top-0 py-10 flex items-center justify-between text-gray-700 dark:text-gray-100">
-            <ul className="flex items-center space-x-8">
-              <li className="inline-flex items-center font-serif font-semibold">
-                <svg
-                  className="w-5 h-5 mr-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                    clipRule="evenodd"
-                  />
-                  <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
-                </svg>
-                Content
-              </li>
-              <li className="inline-flex items-center font-serif font-semibold">
-                <svg
-                  className="w-5 h-5 mr-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                About
-              </li>
-              <li className="inline-flex items-center font-serif font-semibold">
-                <svg
-                  className="w-5 h-5 mr-2"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                Search
-              </li>
-            </ul>
-          </nav>
-          <div className="space-y-5">
+      <header className="grid grid-wrap grid-cols-12 h-screen w-screen bg-gray-50 dark:bg-gray-900">
+        <div className="block lg:hidden">
+          <Navbar />
+        </div>
+        <div className="-mt-16 sm:mt-0 pb-0 sm:py-36 lg:p-10 flex flex-col justify-center relative h-full pl-5 sm:pl-8 md:pl-16 col-span-12 lg:col-span-7">
+          <div className="hidden lg:block">
+            <Navbar />
+          </div>
+          <div className="space-y-3 sm:space-y-5">
             <div className="flex items-center font-serif whitespace-nowrap text-black dark:text-white">
-              <h1 className="font-bold text-5xl md:text-6xl">Pish News</h1>
-              <h2 className="font-semibold text-xl md:text-3xl ml-3 text-coolGray-700 dark:text-coolGray-300 -mb-1">
+              <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl">
+                Pish News
+              </h1>
+              <h2 className="font-semibold text-base sm:text-xl md:text-3xl ml-3 text-coolGray-700 dark:text-coolGray-300 -mb-1">
                 /piSH/
               </h2>
               <button
@@ -98,19 +140,19 @@ const Index: React.FC = () => {
                 )}
               </button>
             </div>
-            <p className="text-lg text-gray-600 dark:text-gray-300 leading-8">
-              <span className="transition ease duration-300 font-semibold hover:text-red-600 dark:hover:text-red-500 text-gray-800 dark:text-gray-400">
+            <p className="text-base sm:text-lg text-gray-600 dark:text-gray-300 leading-7 sm:leading-8 font-semibold">
+              <span className="transition ease duration-300 font-bold hover:text-red-600 dark:hover:text-red-500 text-gray-800 dark:text-gray-400">
                 Exclamation.
               </span>{' '}
-              used to express annoyance, <br className="hidden md:block" />{' '}
+              used to express annoyance, <br className="hidden lg:block" />{' '}
               impatience, or disgust.
             </p>
-            <div className="md:pt-2">
+            <div className="pt-2 sm:pt-0 md:pt-2">
               <Button size="md">Dive in</Button>
             </div>
           </div>
         </div>
-        <div className="col-span-12 lg:col-span-5 w-screen overflow-x-hidden">
+        <div className="col-span-12 lg:col-span-5">
           <img
             src="/assets/circular-city.jpg"
             alt="Photo by sergio souza on Unsplash"
