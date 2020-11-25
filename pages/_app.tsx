cd37286dc46c1d11423c0722e5e351617eaf30bd
coderinblack <kevinlu.email@gmail.com>
2020-11-24 18:44:57 -0800

    Pull chats from firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import Head from 'next/head';
+import firebase from 'firebase';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
 import '../styles/index.css';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCREUIQufBbbSCM74M_51Y3s43xA7Uw2Dw',
+    authDomain: 'pish-8734e.firebaseapp.com',
+    databaseURL: 'https://pish-8734e.firebaseio.com',
+    projectId: 'pish-8734e',
+    storageBucket: 'pish-8734e.appspot.com',
+    messagingSenderId: '946698616937',
+    appId: '1:946698616937:web:4100b32f2c26b8d4c762f8',
+    measurementId: 'G-FDTQXFP09S',
+  });
+  firebase.analytics();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/discuss.tsx
+++ pages/discuss.tsx
@@ -1,19 +1,119 @@
 import React from 'react';
+import { Button } from '../components/Button';
 import { Navbar } from '../components/Navbar';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
+import firebase from 'firebase';
+import ReactTooltip from 'react-tooltip';
 
 const Discuss: React.FC = () => {
+  const auth = firebase.auth();
+  const firestore = firebase.firestore();
+
+  const chatRef = firestore.collection('chats');
+  const chatQuery = chatRef.orderBy('official').limit(25);
+
+  const [user] = useAuthState(auth);
+  const [chats] = useCollectionDataOnce(chatQuery, { idField: 'id' });
+
   return (
-    <div>
+    <div className="text-gray-50 bg-gray-800 min-h-screen">
       <Navbar />
-      <div className="py-36 lg:py-40 px-5 lg:px-0 md:px-10 mx-auto prose-sm lg:prose">
-        <h1 className="font-serif font-bold">Don't Panic, It's Comming Soon</h1>
-        <p className="transform -translate-y-4">
-          The projected MVP publication dates:{' '}
-          <span className="font-bold">
-            November 31, 2020 - December 3, 2020
-          </span>
-        </p>
-      </div>
+      {user ? (
+        <div className="container mx-auto py-28 lg:py-32 px-5">
+          <div className="flex justify-between">
+            <div>
+              <h1
+                data-tip="Official and community driven"
+                className="flex items-center font-bold text-2xl"
+              >
+                Live chats
+              </h1>
+              <ReactTooltip place="bottom" type="dark" effect="float" />
+            </div>
+            <div className="space-x-2">
+              <Button href="/create" sans>
+                Create
+              </Button>
+              <Button
+                onClick={() => auth.signOut()}
+                colorTheme="lightBlue"
+                sans
+              >
+                Sign out
+              </Button>
+            </div>
+          </div>
+          <br />
+          <div className="flex items-center space-x-4">
+            {chats?.map((chat: any) => (
+              <div
+                className="rounded-md bg-gray-700 border border-gray-600 p-5 max-w-sm w-full"
+                key={chat.id}
+              >
+                <h2 className="text-xl font-bold flex items-center">
+                  {chat.name}
+                  <svg
+                    className="w-5 h-5 text-red-500 ml-2.5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"></path>
+                    <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"></path>
+                  </svg>
+                  {chat.official ? (
+                    <svg
+                      className="w-5 h-5 text-lightBlue-500 ml-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : null}
+                </h2>
+                <p
+                  className="mt-2.5 text-gray-400"
+                  style={{ fontSize: '1.075rem' }}
+                >
+                  {chat.description}
+                </p>
+                <div className="mt-5">
+                  <Button>
+                    <div className="px-4">Join</div>
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="py-36 lg:py-40 px-5 lg:px-0 md:px-10 mx-auto max-w-prose space-y-3">
+          <h1 className="font-serif font-bold text-4xl">
+            Discuss with the community
+          </h1>
+          <p className="text-lg">
+            Please begin by choosing a{' '}
+            <span className="font-bold">social account provider</span>
+          </p>
+          <div className="pt-3">
+            <Button
+              onClick={() => {
+                const provider = new firebase.auth.GoogleAuthProvider();
+                auth.signInWithPopup(provider);
+              }}
+              sans
+            >
+              Continue with Google
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
